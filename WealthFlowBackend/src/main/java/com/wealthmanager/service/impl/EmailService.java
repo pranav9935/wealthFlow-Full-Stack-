7ab71d7a7ff2fff6b3411322934be409f0315462
pranav9935 <147136908+pranav9935@users.noreply.github.com>
2026-03-15 23:45:17 +0530

    "Changed to Gmail OTP"

--- WealthFlowBackend/src/main/java/com/wealthmanager/service/impl/EmailService.java
+++ WealthFlowBackend/src/main/java/com/wealthmanager/service/impl/EmailService.java
@@ -1,48 +1,31 @@
 package com.wealthmanager.service.impl;
 
 import org.springframework.http.*;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import lombok.RequiredArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
-    private final String API_KEY = System.getenv("RESEND_API_KEY");
+    private final JavaMailSender mailSender;
 
     public void sendOtp(String email, String otp) {
 
-        RestTemplate restTemplate = new RestTemplate();
+        SimpleMailMessage message = new SimpleMailMessage();
 
-        String url = "https://api.resend.com/emails";
+        message.setFrom("pranavmishra9807@gmail.com");
+        message.setTo(email);
+        message.setSubject("WealthFlow Email Verification");
+        message.setText("Your OTP is: " + otp);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer " + API_KEY);
-
-        Map<String, Object> body = new HashMap<>();
-        body.put("from", "onboarding@resend.dev");
-        body.put("to", email);
-        body.put("subject", "WealthFlow Email Verification");
-        body.put("text", "Your OTP is: " + otp);
-
-        HttpEntity<Map<String, Object>> request =
-                new HttpEntity<>(body, headers);
-
-        try {
-
-            ResponseEntity<String> response =
-                    restTemplate.postForEntity(url, request, String.class);
-
-            System.out.println("Email API response: " + response.getBody());
-
-        } catch (Exception e) {
-
-            System.out.println("Email sending failed:");
-            e.printStackTrace();
-
-        }
+        mailSender.send(message);
     }
 }
