f8317d6f9a161911ceea45fa521b0c79c2ee51f6
pranav9935 <147136908+pranav9935@users.noreply.github.com>
2026-03-15 22:31:26 +0530

    "added Async and changes port number of email"

--- WealthFlowBackend/src/main/java/com/wealthmanager/WealthManagementApplication.java
+++ WealthFlowBackend/src/main/java/com/wealthmanager/WealthManagementApplication.java
@@ -3,10 +3,12 @@ package com.wealthmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 @EnableKafka
+@EnableAsync
 public class WealthManagementApplication {
 
 	public static void main(String[] args) {
--- WealthFlowBackend/src/main/java/com/wealthmanager/service/impl/EmailService.java
+++ WealthFlowBackend/src/main/java/com/wealthmanager/service/impl/EmailService.java
@@ -3,6 +3,7 @@ package com.wealthmanager.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final JavaMailSender mailSender;
-
+     @Async
     public void sendOtp(String email, String otp) {
 
         SimpleMailMessage message = new SimpleMailMessage();
